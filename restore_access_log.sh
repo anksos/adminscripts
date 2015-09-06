5cacc52be6b317fec76ec23a02fd547254d756de
Anastasios Ksouzafeiris <anastasis.ksouzafeiris@gmail.com>
2015-09-06 15:05:51 +0300

    added new file on adminscripts is more like a mini guideline.

---
+++ restore_access_log.sh
@@ -0,0 +1,17 @@
+# Restore apache access_logs if they have been deleted
+#
+# Scenario:
+
+$ su -
+$ cd /var/log/httpd
+$ rm -f access_log
+
+# Solution:
+
+$ ps aux | grep httpd	# note the PID of the main Apache process
+$ ls -lsa /proc/1784/fd	# fd = a directory containing all of the file descriptors for a particular process
+# we will find something like this: /var/log/httpd/access_log (deleted) and in front of it a 'X' number
+$ service httpd stop	# stop the httpd service
+$ cp /proc/1784/fd/7 /var/log/httpd/access_log
+$ service httpd start
+$ tail -f /var/log/httpd/access_log 	# to check if we restore it without problem
